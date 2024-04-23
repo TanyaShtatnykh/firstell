10eef7bf12672b50e51a95b28181159e3749ff0c
Tanya Shtatnykh <tanya.shtatnykh@gmail.com>
2024-04-23 14:05:45 +0700

    portdolio added

--- src/blocks/portfolio/portfolio.js
+++ src/blocks/portfolio/portfolio.js
@@ -1,10 +1,3 @@
-
-$(function () {
-  $('.header__burger-btn').on('click', function() {
-    $(this).toggleClass('header__burger-btn--open');
-    $('.header__menu').toggleClass('header__menu--open');
-  })
-});
 $(function () {
   $('.owl-carousel').owlCarousel({
     items: 1,
@@ -27,12 +20,3 @@ $(function () {
     }
   });
 });
-
-$(function () {
-  $('.services__btn').on('click', function() {
-    $('.service').toggleClass('service--open');
-    $('.services__btn').toggleClass('services__btn--open');
-  });
-});
-
-
